07e831ac8207baa3f0b37507ba7a79a30f35518b
jananikavindi <jananikavindi56@gmail.com>
2026-08-12 03:02:00 +0530

    Add files via upload

---
+++ Calculate Average Marks of 4 Subjects.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+float marks,average;
+cout<<"enter the marks 1:";
+cin>> marks ;
+cout<<"enter the marks 2:";
+cin>> marks ;
+cout<<"enter the marks 3:";
+cin>> marks ;
+cout<<"enter the marks 4:";
+cin>> marks ;
+average=( marks + marks + marks + marks )/4;
+cout<<"average="<<average;
+return 0;
+}
+
