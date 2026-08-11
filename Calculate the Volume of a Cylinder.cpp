76d9f4239ed5a16ba7e052e2c43f5ce9aac798c7
jananikavindi <jananikavindi56@gmail.com>
2026-08-12 03:05:58 +0530

    Add files via upload

---
+++ Calculate the Volume of a Cylinder.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+float r,h,volum;
+const float PI=3.1415;
+cout << "enter the r:";
+cin>> r;
+cout << "enter the h:";
+cin>> h;
+volum=PI*r*h;
+cout<<"volum="<<volum;
+return 0;
+}
+
