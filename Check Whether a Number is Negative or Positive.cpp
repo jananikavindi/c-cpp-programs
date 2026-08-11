4fe44a4c97b0b5c1b05347003156cece01cc1fb7
jananikavindi <jananikavindi56@gmail.com>
2026-08-12 03:06:34 +0530

    Add files via upload

---
+++ Check Whether a Number is Negative or Positive.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int number;
+cout<<"enter the number:";
+cin>> number;
+if(number==0){
+cout<<"number is zero";
+}
+else if(number>0){
+cout<<"number is positive";
+}
+else
+{
+ cout<<"number is negative";
+}
+
+ return 0;
+}
+
